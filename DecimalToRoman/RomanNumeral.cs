c5df68796dc2627ab4d5947a56c9cd1058a8b11d
Daniel Stack <dstack@microsoft.com>
2017-10-07 15:30:10 -0700

    failing tests

---
+++ DecimalToRoman/RomanNumeral.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DecimalToRoman
+{
+    public class RomanNumeral
+    {
+        string _numeral;
+        public RomanNumeral(string numeral)
+        {
+            _numeral = numeral;
+        }
+        public int ToDecimal()
+        {
+            return 0;
+        }
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using DecimalToRoman;
+using Xunit;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("I", 1)]
+        [InlineData("IV", 4)]
+        [InlineData("V", 5)]
+        [InlineData("VI", 6)]
+        [InlineData("X", 10)]
+        [InlineData("L", 50)]
+        [InlineData("C", 100)]
+        [InlineData("D", 500)]
+        [InlineData("M", 1000)]
+        public void DDT(string numeral, int expected)
+        {
+            RomanNumeral roman = new RomanNumeral(numeral);
+            Assert.Equal(expected, roman.ToDecimal());
+        }
+    }
+}
